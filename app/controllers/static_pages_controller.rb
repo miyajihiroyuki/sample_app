8cc4ab3721fc68b64b500d0866a1ace9706f03fc
Hiro Miyaji <miyajihiroyuki@yahoo.co.jp>
2016-06-19 17:01:14 +0900

    Finish 10.3.2

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,11 @@
 class StaticPagesController < ApplicationController
+#  def home
+#    @micropost = current_user.microposts.build if signed_in?
+#  end
   def home
+    if signed_in?
+      @micropost = current_user.microposts.build
+    end
   end
 
   def help
